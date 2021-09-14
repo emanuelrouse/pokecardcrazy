bd645debc7dc0065bf8f838cc9c62c4ca95cdf61
Emanuel Rouse <emanuelrouse@hotmail.com>
2021-09-14 18:08:20 -0400

    Initial Config

---
+++ src/components/Main.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class Main extends React.Component {
+    render() {
+        return (
+            <h1>Hello World</h1>
+        )
+    }
+}
+
+export default Main;
